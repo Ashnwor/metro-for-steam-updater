028f8c11a52a6b0a2ab9d77a37bda2de9fdaa99b
Ashnwor <ashnwor@gmail.com>
2019-09-25 03:40:54 +0300

    better approach for finding url

--- mfsu.py
+++ mfsu.py
@@ -10,33 +10,53 @@ urlUnofficial = "https://github.com/redsigma/UPMetroSkin/archive/master.zip"
 skinsLocation = "/home" + "/" + getlogin() + "/.steam/steam/skins/metro-for-steam"
 unofficialPatchFileName = "unofficial_patch.zip"
 linkList = []
+actualFileList=[]
+search = True
+searchForSlash = True
 
 subprocess.run(["wget", urlIndex])
 index = open("index.html", "rt")
 context = index.read()
 index.close()
-start = context.find("downloads/")
-fileStart = start + 10
+# Search for certain string in the index file
+start = context.find(".zip")
+searchIndex = start
+
+while search:
+    searchIndex = searchIndex-1
+    varIndex = context[searchIndex]
+    if searchForSlash == True:
+        if varIndex == "/":
+            fileNameStart = searchIndex+1
+            searchForSlash = False
+            print("Filename start:", fileNameStart)
+    if varIndex == "\"":
+        searchIndex = searchIndex+1
+        break
+fileStart = searchIndex
 print("file start index:", context[fileStart])
 print("starting index:", start)
 print(context[start])
-end = context.find("\"",start)
+end = context.find(".zip")+4
+for x in range(end-fileNameStart):
+    actualFileList.append(context[fileNameStart+x])
+actualFile = ''.join(actualFileList)
+print(actualFileList)
+print(actualFile)
 print("ending index:", end)
 print(context[end-1])
 for x in range(end-fileStart):
     linkList.append(context[fileStart+x])
 link = ''.join(linkList)
 print(link)
-linkUrl = urlAddress + "downloads/" + link
-print(linkUrl)
 print(unofficialPatchFileName)
 
 # download theme
-subprocess.run(["wget", linkUrl])
+subprocess.run(["wget", link])
 # download unofficial patch
 subprocess.run(["wget", "-O", unofficialPatchFileName, urlUnofficial])
 # extract theme to temp folder 
-with ZipFile(link, "r") as themeZip:
+with ZipFile(actualFile, "r") as themeZip:
     themeZip.extractall("temp")
 # extract unofficial patch to tempPatch folder
 with ZipFile(unofficialPatchFileName, "r") as zipUnofficialPatch:
@@ -66,7 +86,7 @@ if "Extras" in thisMenu:
     subprocess.run(["cp", "-avrfa", "Extras/.", skinsLocation])
 
 # delete unnecessary files
-subprocess.run(["rm", "index.html", link, unofficialPatchFileName])
+subprocess.run(["rm", "index.html", actualFile, unofficialPatchFileName])
 subprocess.run(["rm", "-rf", "temp", "tempPatch"])
 
 
